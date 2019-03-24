1e4d77725247a9fed1bd7c15a5a02f5a0f7f4174
Jinthushan <apex@Jinthushans-MacBook-Pro.local>
2019-03-24 13:05:35 +0530

    signup front end

--- app/src/main/java/com/example/jurist/MainActivity.java
+++ app/src/main/java/com/example/jurist/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.jurist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private static Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onClickButtonLister();
+    }
+
+    public void onClickButtonLister(){
+        signup = (Button)findViewById(R.id.lawyer_sign_up);
+        signup.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(".createPassword");
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
---
+++ app/src/main/java/com/example/jurist/createPassword.java
@@ -0,0 +1,13 @@
+package com.example.jurist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class createPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_password);
+    }
+}
